42edf6d599984561b069d3cb57f0c65e3355f9c2
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-10-17 23:09:01 -0300

    atualizando conhecimento

---
+++ Migrations/20221018014910_PopulaProdutos.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaProdutos : Migration
+    {
+    protected override void Up(MigrationBuilder mb)
+    {
+      mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES ('Coca-Cola Diet', 'Refrigerange de Cola 350ml', 5.45, 'cocacola.jpg', 50, now(), 1)");
+      mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES ('Lanche de Atum', 'Lanche de Atum com maionese', 8.50, 'atum.jpg', 10, now(), 2)");
+      mb.Sql("INSERT INTO Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES ('Pudim 100g', 'Pudim de leite condensado 100g', 6.75, 'pudim.jpg', 20, now(), 3)");
+    }
+
+    protected override void Down(MigrationBuilder mb)
+    {
+      mb.Sql("DELETE FROM Produtos");
+    }
+  }
+}
