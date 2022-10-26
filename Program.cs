e57bd81e993a69600cefa998ed63790e29c46f10
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-10-26 00:48:19 -0300

    att conhecimento

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// Foi adicionado essa funcionalidade "middleware" para ignorar cenários de erro quando temos
+// 1 entidade Produto dentro da entidade Categoria e uma entidade Categoria dentro de produto,
+// ele começa a dar erro nesses cenários e a solução é ignorar esses ciclos duplicados
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+      options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
+    );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 string mysqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
